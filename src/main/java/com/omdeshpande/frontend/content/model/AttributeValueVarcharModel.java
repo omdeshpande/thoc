dd70ba5c3cdfcbf0646fd1f1aa18002d921bd744
Om Deshpande <om.deshpande@razorfish.com>
2016-06-26 16:44:47 +0530

    Added interfaces, abstracts for int and varchar attribute values + hibernate mappings

---
+++ src/main/java/com/omdeshpande/frontend/content/model/AttributeValueVarcharModel.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.omdeshpande.frontend.content.model;
+
+import com.omdeshpande.lib.abstracts.AttributeValueAbstract;
+
+/**
+ * @author Om Deshpande
+ *
+ */
+public class AttributeValueVarcharModel extends AttributeValueAbstract {
+	
+	
+}
